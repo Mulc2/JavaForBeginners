835c26984b1177eeef79262354a3ba4b282516f0
Mulc2 <smulcrow@utcmediacityuk.org.uk>
2016-09-13 10:46:28 +0100

    Add files via upload

---
+++ AlwaysMakeANewProject.java
@@ -0,0 +1,46 @@
+
+package alwaysmakeanewproject;
+
+public class AlwaysMakeANewProject {
+
+    public static void main(String[] args) {
+    String colour = "purple"; //use speech marks and accepts integer and letters
+    String name = "Saul";
+    String street = "Carlton Close";
+        System.out.println("my favourite colour is " + colour); //use sout and tab, leave space after statement
+        System.out.println("My name is " + name);
+        System.out.println("I grew up on " + street);
+        
+        int number = 5; //if integer use int no speech marks needed
+        int age = 16;
+        int shoe = 8;
+        System.out.println("My chosen number is " + number);
+        System.out.println("My age is " + age);
+        System.out.println("My shoe size is " + shoe);
+        
+        double test = 1.5; //double if decimal
+        double pi = 3.141;
+        System.out.println(test);
+        System.out.println(pi);
+        
+        //area of circle = pi*r^2
+        //speed = distance/time
+        
+        System.out.println(age + shoe);
+        System.out.println(age - shoe);
+        System.out.println(age * shoe);
+        System.out.println(age / shoe);
+        
+        int x = 10;
+        int y = 5;
+        if (x < y) {
+            System.out.println("x is lower than y");
+        //if the condition is true    
+        }else{
+            System.out.println("x is higher than y");
+        //if the condition is false
+        }
+        
+    }
+   
+}
